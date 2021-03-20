9a33283ac7f0ce79f00a9ef089c1d59d3cb454f8
Lv isbl <s3879364@rmit.edu.vn>
2021-03-20 07:25:14 +0000

    change

--- assignment1/mode.h
+++ assignment1/mode.h
@@ -4,7 +4,7 @@ using namespace std;
 #define MAX 1000000
 void mode(int* &arr, int size) {
 	int highest = 0;
-	int* count = new int[size];
+	int* count = new int[MAX];
 
 	// count the number of repetion of each element
 	for (int i = 0; i < size; i++) {
@@ -14,7 +14,7 @@ void mode(int* &arr, int size) {
 
 	// print the most frequently occurring elements
     cout << "{";
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < MAX; i++) {
 		if (highest == count[i]) cout << " " << i << " ";
 	}
     cout << "}";
